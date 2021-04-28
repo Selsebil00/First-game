1993ededf19e9c15380c4a001a0c1db18d890830
Selsebil <56333411+Selsebil00@users.noreply.github.com>
2021-04-28 17:28:42 +0200

    go

---
+++ First-game/Assets/Scripts/Hexagon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hexagon : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float theSpeed = 3f;    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.rotation = Random.Range(0f, 360f);
+        transform.localScale = Vector3.one * 10f; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale-= Vector3.one* theSpeed *Time.deltaTime ;
+           if (transform.localScale.x <= 0.05f)
+        {
+            Destroy(gameObject);
+        } 
+
+
+    }
+}
---
+++ First-game/Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{   // vitesse de notre player 
+   
+   
+    
+
+    private Vector3 touchPosition;
+    private Rigidbody2D rb;
+    private Vector3 direction;
+    private float moveSpeed=10f  ;
+
+
+
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // movement = Input.GetAxisRaw("Horizontal"); 
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            touchPosition.z = 0;
+            direction = (touchPosition - transform.position);
+            rb.velocity = new Vector2(direction.x, direction.y) * moveSpeed;
+            if (touch.phase == TouchPhase.Ended)
+                rb.velocity = Vector2.zero;
+        }
+    }
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
